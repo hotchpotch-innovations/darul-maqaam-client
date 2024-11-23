bd9d05681ff095d85178ca6f21a78251747bb8ef
dev2-hotchpotch <dev2.hotchpotchinnovations@gmail.com>
2024-11-23 16:40:55 +0600

    feat: create userSlice for distribute own profilr data

--- src/redux/hooks.ts
+++ src/redux/hooks.ts
@@ -1,11 +1,10 @@
+import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import type { RootState, AppDispatch } from "./store";
 
 // Use throughout your app instead of plain `useDispatch` and `useSelector`
 export const useAppDispatch = useDispatch.withTypes<AppDispatch>();
 export const useAppSelector = useSelector.withTypes<RootState>();
-
-import { useEffect, useState } from "react";
 
 interface UseDebouncedProps {
   searchQuery: string;

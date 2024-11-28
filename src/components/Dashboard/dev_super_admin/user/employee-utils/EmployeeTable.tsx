3cabd847aeeff9d3abc7f7b301f62a675b4fb93f
Md Rakibul Hasan <hello.rakibh@gmail.com>
2024-11-28 13:45:48 +0600

    fix: manage employee table responsive

--- src/components/Dashboard/dev_super_admin/user/employee-utils/EmployeeTable.tsx
+++ src/components/Dashboard/dev_super_admin/user/employee-utils/EmployeeTable.tsx
@@ -7,7 +7,8 @@ import {
   useGetAllEmployeeQuery,
 } from "@/redux/api/user/employeeApi";
 import { useDebounced } from "@/redux/hooks";
-import { Box, Stack, Tooltip, Typography } from "@mui/material";
+import { Box, Tooltip, Typography } from "@mui/material";
+import Grid from "@mui/material/Grid2";
 import { DataGrid, GridColDef } from "@mui/x-data-grid";
 import Image from "next/image";
 import { useState } from "react";
@@ -233,31 +234,31 @@ const EmployeeTable = () => {
     setLimit(newPaginationModel.pageSize);
   };
   return (
-    <Box sx={{ m: "30px 60px" }}>
-      <Stack direction="row" justifyContent="space-between" mb={2}>
-        <Box
-          sx={{
-            display: "flex",
-            gap: "30px",
-          }}
-        >
+    <Box sx={{ p: 2 }}>
+      <Grid container spacing={2} sx={{ mt: 2 }}>
+        <Grid size={{ xs: 12, lg: 4 }}>
           <SelectFilter
             filter_title="Search by department"
             options={department_options}
             value={departmentId}
             setValue={setDepartment}
+            fullWidth
           />
-
+        </Grid>
+        <Grid size={{ xs: 12, lg: 4 }}>
           <SelectFilter
             filter_title="Search by designation"
             options={designation_options}
             value={designationId}
             setValue={setDesignation}
             isDisable={departmentId || designation_isLoading ? false : true}
+            fullWidth
           />
-        </Box>
-        <SearchFiled setSearchText={setSearchTerm} />
-      </Stack>
+        </Grid>
+        <Grid size={{ xs: 12, lg: 4 }} mt={{ xs: 0, lg: 4 }}>
+          <SearchFiled setSearchText={setSearchTerm} />
+        </Grid>
+      </Grid>
 
       {!isLoading ? (
         <Box>

bd1495ae7c7eadcd26e4ddeec3fc57b41c524dbe
Md Rakibul Hasan <hello.rakibh@gmail.com>
2024-11-30 16:32:05 +0600

    fix: spacing on departmetn table

--- src/components/Dashboard/dev_super_admin/settings/department-utils/DepartmentTable.tsx
+++ src/components/Dashboard/dev_super_admin/settings/department-utils/DepartmentTable.tsx
@@ -177,8 +177,13 @@ const DepartmentTable = () => {
   };
   return (
     <Box>
-      <Box sx={{ m: "30px 60px" }}>
-        <Stack direction="row" justifyContent="space-between" mb={2}>
+      <Box sx={{ p: 2 }}>
+        <Stack
+          direction="row"
+          justifyContent="space-between"
+          mb={2}
+          spacing={2}
+        >
           <Box
             sx={{
               display: "flex",

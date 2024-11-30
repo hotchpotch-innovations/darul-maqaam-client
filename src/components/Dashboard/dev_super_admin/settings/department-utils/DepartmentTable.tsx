cd985bcc344db1647edb6d9d47a2cd76e67b3859
Md Rakibul Hasan <hello.rakibh@gmail.com>
2024-11-30 16:48:41 +0600

    fix: route to create department & create designation

--- src/components/Dashboard/dev_super_admin/settings/department-utils/DepartmentTable.tsx
+++ src/components/Dashboard/dev_super_admin/settings/department-utils/DepartmentTable.tsx
@@ -42,7 +42,7 @@ const DepartmentTable = () => {
   //Modal Functionality Is End
 
   const path_create_country =
-    "/dashboard/super_admin/users/settings/department/create";
+    "/dashboard/dev_super_admin/users/settings/department/create";
 
   const [currentPage, setCurrentPage] = useState(1);
 
